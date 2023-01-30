7085e6efdc17404e755f33b9f20f5c6eafa21a2c
Imran ICT <emon08ctg@gmail.com>
2023-01-30 17:14:26 +0600

    tasbeeh function

--- lib/CounterApp/counter.dart
+++ lib/CounterApp/counter.dart
@@ -34,14 +34,9 @@ class _CounterDashState extends State<CounterDash> {
 
   var tasbihName = "Tasbeeh Counter";
 
-//var  counter=0;
-  void tasbeehCounter({int? count1, int? count2, int? count3}) {
-    count1;
-    count2;
-    count3;
-  }
-
   int result = 0;
+  int totalCount=0;
+
   int subhanaAllah = 0;
   int alhamdulillah = 0;
   int laIlahaIllallah = 0;
@@ -73,21 +68,36 @@ crossAxisAlignment: CrossAxisAlignment.start,
                         end: Alignment.bottomRight
                     )
                 ),
-                child: Row(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+
                   children: [
                     Expanded(
-                      flex: 3,
-                      child: Padding(
-                          padding: const EdgeInsets.all(0),
-                          child: Text(tasbihName, style: TextStyle(fontSize: 40,fontWeight: FontWeight.normal,color: Colors.lightGreenAccent,fontFamily: 'ds-digi'),textAlign: TextAlign.right,)),
-                    ),
-
-                    //Result Display
+                      flex:1,
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 15, 20, 0),
+                          child: Text("Total Count:  " + totalCount.toString(),style: TextStyle(fontSize: 15,fontWeight: FontWeight.normal,color: Colors.lightGreenAccent,fontFamily: 'ds-digi'),),
+                        )),
                     Expanded(
-                      flex: 1,
-                      child: Padding(
-                          padding: const EdgeInsets.only(right: 20),
-                          child: Text(result.toString(),style: TextStyle(fontSize: 50,fontWeight: FontWeight.normal,color: Colors.lightGreenAccent,fontFamily: 'ds-digi'),textAlign: TextAlign.right,)),
+                      flex: 3,
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: Padding(
+                                padding: const EdgeInsets.all(0),
+                                child: Text(tasbihName, style: TextStyle(fontSize: 40,fontWeight: FontWeight.normal,color: Colors.lightGreenAccent,fontFamily: 'ds-digi'),textAlign: TextAlign.right,)),
+                          ),
+
+                          //Result Display
+                          Expanded(
+                            flex: 1,
+                            child: Padding(
+                                padding: const EdgeInsets.only(right: 20),
+                                child: Text(result.toString(),style: TextStyle(fontSize: 50,fontWeight: FontWeight.normal,color: Colors.lightGreenAccent,fontFamily: 'ds-digi'),textAlign: TextAlign.right,)),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
@@ -98,80 +108,86 @@ crossAxisAlignment: CrossAxisAlignment.start,
             child: Text('Counting History',style: TextStyle(fontSize: 15,fontWeight: FontWeight.normal,color: Colors.black54,),textAlign: TextAlign.start,)
           ),
 
-          Row(
-            children: [
-              //SubhanaAllah
-              Expanded(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          subhanaAllah.toString(),
-                          style: TextStyle(fontSize: 20,fontWeight: FontWeight.normal,color: Colors.orange,fontFamily: 'ds-digi'),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          //SubhanaAllah
-                          'SUBHANALLAH',
-                          style: TextStyle(fontSize: 15,fontWeight: FontWeight.normal,color: Colors.orange,),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: Row(
+              children: [
+                //SubhanaAllah
+                Expanded(
+                  flex: 3,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            subhanaAllah.toString(),
+                            style: TextStyle(fontSize: 20,fontWeight: FontWeight.normal,color: Colors.orange,fontFamily: 'ds-digi'),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            //SubhanaAllah
+                            'SUBHANALLAH',
+                            style: TextStyle(fontSize: 12,fontWeight: FontWeight.normal,color: Colors.orange,),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              //Alhamdulillah
-              Expanded(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          alhamdulillah.toString(),
-                          style: TextStyle(fontSize: 20,fontWeight: FontWeight.normal,color: Colors.blue,fontFamily: 'ds-digi'),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          //SubhanaAllah
-                          'ALHAMDULILLAH',
-                          style: TextStyle(fontSize: 15,fontWeight: FontWeight.normal,color: Colors.blue,),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
+                //Alhamdulillah
+                Expanded(
+                  flex: 3,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            alhamdulillah.toString(),
+                            style: TextStyle(fontSize: 20,fontWeight: FontWeight.normal,color: Colors.blue,fontFamily: 'ds-digi'),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            //SubhanaAllah
+                            'ALHAMDULILLAH',
+                            style: TextStyle(fontSize: 12,fontWeight: FontWeight.normal,color: Colors.blue,),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
 
-              //La Ilaha Illah
-              Expanded(
-                child: Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          subhanaAllah.toString(),
-                          style: TextStyle(fontSize: 20,fontWeight: FontWeight.normal,color: Colors.green,fontFamily: 'ds-digi'),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          //SubhanaAllah
-                          'SUBHANALLAH',
-                          style: TextStyle(fontSize: 15,fontWeight: FontWeight.normal,color: Colors.green,),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
+                //La Ilaha Illah
+                Expanded(
+                  flex: 4,
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            laIlahaIllallah.toString(),
+                            style: TextStyle(fontSize: 20,fontWeight: FontWeight.normal,color: Colors.green,fontFamily: 'ds-digi'),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            //SubhanaAllah
+                            'LA-ILAHA-ILLALLAH',
+                            style: TextStyle(fontSize: 12,fontWeight: FontWeight.normal,color: Colors.green,),
+                            textAlign: TextAlign.center,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
 
 
@@ -190,9 +206,13 @@ crossAxisAlignment: CrossAxisAlignment.start,
                     setState(() {
                       count1++;
                       result = count1;
+                      subhanaAllah=count1;
+                      totalCount=count1+count2+count3;
+
                     });
                     alhamdulillah = count2;
                     laIlahaIllallah = count3;
+
                   },
                   style: ElevatedButton.styleFrom(
                       padding: EdgeInsets.all(20), backgroundColor: Colors.orange),
@@ -216,9 +236,11 @@ crossAxisAlignment: CrossAxisAlignment.start,
                     setState(() {
                       count2++;
                       result = count2;
+                      alhamdulillah=count2;
+                      totalCount=result;
                     });
                     subhanaAllah = count1;
-                    laIlahaIllallah = count3;
+                    totalCount=count1+count2+count3;
                   },
                   style: ElevatedButton.styleFrom(
                       padding: EdgeInsets.all(20),
@@ -237,8 +259,10 @@ crossAxisAlignment: CrossAxisAlignment.start,
                   onPressed: () {
                     tasbihName = "LA-ILAHA-ILLALLAH";
                     setState(() {
-                      tasbeehCounter(count2: count3++);
+                      count3++;
                       result = count3;
+                      laIlahaIllallah=count3;
+                      totalCount=count1+count2+count3;
                     });
                     subhanaAllah = count1;
                     alhamdulillah = count2;
@@ -269,6 +293,7 @@ crossAxisAlignment: CrossAxisAlignment.start,
                       alhamdulillah = 0;
                       laIlahaIllallah = 0;
                       result = 0;
+                      totalCount=0;
                     });
                   },
                   style: ElevatedButton.styleFrom(
